9b05410db9f4790448b012cbb08759cea0514691
jkloppenburg1 <jkloppenburg1@student.cccd.edu>
2016-09-13 17:30:47 -0700

    Copied from usb

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/tapcounter/Counter.java
@@ -0,0 +1,29 @@
+package edu.orangecoastcollege.cs273.tapcounter;
+
+/**
+ * Created by Justaco on 9/11/2016.
+ */
+public class Counter
+{
+    private int numberOfTaps;
+
+    public Counter()
+    {
+        this.numberOfTaps = 0;
+    }
+
+    public Counter(int numberOfTaps)
+    {
+        this.numberOfTaps = numberOfTaps;
+    }
+
+    public int getNumberOfTaps()
+    {
+        return numberOfTaps;
+    }
+
+    public void addNumberOfTaps()
+    {
+        this.numberOfTaps++;
+    }
+}
--- app/src/main/java/edu/orangecoastcollege/cs273/tapcounter/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/tapcounter/MainActivity.java
@@ -2,12 +2,31 @@ package edu.orangecoastcollege.cs273.tapcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button speakButton;
+    private final Counter myCounter = new Counter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.countTextView);
+
+        speakButton = (Button) findViewById(R.id.tapButton);
+
+        speakButton.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    public void onClick(View view)
+                    {
+                        myCounter.addNumberOfTaps();
+                    }
+                }
+        );
     }
 }
